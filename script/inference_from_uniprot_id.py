4e2f5c355926296cf321e211de0017597a1a9620
王晓瑞 <wangxr2018@lzu.edu.cn>
2025-02-13 17:27:22 +0800

    update inference script

--- script/inference_from_uniprot_id.py
+++ script/inference_from_uniprot_id.py
@@ -3,6 +3,7 @@ import os
 import argparse
 import torch
 import pandas as pd
+from rdkit import Chem
 sys.path.append('../')
 from tqdm import tqdm
 from webapp.utils import UniProtParserMysql, EasIFAInferenceAPI, retrain_ec_site_model_state_path, cmd
@@ -15,42 +16,78 @@ def inference_from_uniprot_ids(uniprot_id_list: list, ECSitePred: EasIFAInferenc
     for uniprot_id in tqdm(uniprot_id_list):
         query_data, _, _ = unprot_mysql_parser.query_from_uniprot(uniprot_id)
         uniprot_id, query_results_df, msg, _ = query_data
-        query_results_df['UniProt ID'] = [uniprot_id for _ in range(len(query_results_df))]
+
         if query_results_df is None:
+            query_results_df = pd.DataFrame({
+                'uniprot_id': [uniprot_id],  
+                'ec': [None],                
+                'rxn_smiles': [None],        
+                'pdb_fpath': [None],         
+                'aa_length': [None],         
+                'predicted_results': [[]],
+                'predicted_results': [msg],
+            })
             msg = uniprot_id + ': ' + msg 
             print(msg)
+            all_results_df = pd.concat([all_results_df, query_results_df], axis=0)
             continue
-
+    
+        query_results_df['uniprot_id'] = [uniprot_id for _ in range(len(query_results_df))]
         enzyme_aa_length = query_results_df["aa_length"].tolist()[0]
         if ECSitePred.max_enzyme_aa_length < enzyme_aa_length:
-            query_results_df['predicted_results'] = [None for _ in range(len(query_results_df))]
-            print(uniprot_id + ' sequence is too long! pass! (☼Д☼)')
+            print(uniprot_id + ': ' + 'Sequence is too long, pass! (☼Д☼)')
+            query_results_df['predicted_results'] = [[] for _ in range(len(query_results_df))]
+            query_results_df['messages'] = ['Sequence is too long' for _ in range(len(query_results_df))]
+            all_results_df = pd.concat([all_results_df, query_results_df], axis=0)
             continue
-
+        
         predicted_results = []
-
-        for idx, row in enumerate(query_results_df.itertuples()):
+        messages = []
+        for idx, row in enumerate(query_results_df.itertuples()): 
             rxn = row[2]
             enzyme_structure_path = row[3]
-            if not os.path.exists(enzyme_structure_path):
-                enzyme_structure_path = os.path.join(
-                    unprot_mysql_parser.unprot_parser.alphafolddb_folder,
-                    f"AF-{uniprot_id}-F1-model_v4.pdb",
-                )
-                cmd(
-                    unprot_mysql_parser.unprot_parser.download_alphafolddb_url_template.format(
-                        enzyme_structure_path, uniprot_id
-                    )
-                )
+            try:
 
-            pred_active_site_labels = ECSitePred.inference(
-                rxn=rxn, enzyme_structure_path=enzyme_structure_path
-            )
-            del ECSitePred.caculated_sequence
-            predicted_results.append(pred_active_site_labels)
+                if not rxn or not isinstance(rxn, str):
+                    raise ValueError(f"Invalid rxn")
+                if not os.path.exists(enzyme_structure_path):
+                    enzyme_structure_path = os.path.join(
+                        unprot_mysql_parser.unprot_parser.alphafolddb_folder,
+                        f"AF-{uniprot_id}-F1-model_v4.pdb",
+                    )
+                    cmd(
+                        unprot_mysql_parser.unprot_parser.download_alphafolddb_url_template.format(
+                            enzyme_structure_path, uniprot_id
+                        )
+                    )
+
+                try:
+                    mol = Chem.MolFromPDBFile(enzyme_structure_path)
+                    if mol is None:
+                        raise ValueError("PDB not valid")
+                except Exception as e:
+                    raise ValueError(str(e))
+                
+                try:
+                    pred_active_site_labels = ECSitePred.inference(
+                        rxn=rxn, enzyme_structure_path=enzyme_structure_path
+                    )
+                    del ECSitePred.caculated_sequence
+                    if pred_active_site_labels is None:
+                        raise ValueError("Inference erro, check gcc, g++ and conda envs")
+                except Exception as e:
+                    raise ValueError(str(e))
+                predicted_results.append(pred_active_site_labels)
+                messages.append('SUCC')
+            except Exception as e:
+                print(uniprot_id + ': ' + str(e))
+                predicted_results.append([])
+                messages.append(str(e))
+            
         query_results_df['predicted_results'] = predicted_results
+        query_results_df['messages'] = messages
         all_results_df = pd.concat([all_results_df, query_results_df], axis=0)
-    all_results_df = all_results_df[['UniProt ID', 'ec','rxn_smiles','pdb_fpath','aa_length','predicted_results']]
+    all_results_df = all_results_df[['uniprot_id', 'ec','rxn_smiles','pdb_fpath','aa_length','predicted_results', 'messages']]
     return all_results_df
 
 def read_uniprot_ids_from_file(file_path: str):
